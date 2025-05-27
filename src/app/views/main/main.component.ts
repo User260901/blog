38cd41697295cee2bcdbe1768530498a1ecd230d
User260901 <alexmofinadze0@gmail.com>
2025-05-27 07:36:58 +0400

    finish main page

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -47,13 +47,11 @@ export class MainComponent implements OnInit{
   serverStaticPath = environment.serverStaticPath
   customOptions: OwlOptions = {
     loop: true,
-    mouseDrag: false,
+    mouseDrag: true,
     touchDrag: false,
     pullDrag: false,
-    dots: false,
+    dots:false,
     navSpeed: 700,
-
-    navText: ['', ''],
     responsive: {
       0: {
         items: 1
@@ -64,12 +62,26 @@ export class MainComponent implements OnInit{
       740: {
         items: 3
       },
-      940: {
-        items: 4
-      }
     },
-    nav: true
+    nav: false,
   }
+  reviews = [
+    {
+      image: 'review1.png',
+      name: 'Станислав',
+      review: 'Спасибо огромное АйтиШторму за прекрасный блог с полезными статьями! Именно они и побудили меня углубиться в тему SMM и начать свою карьеру.'
+    },
+    {
+      image: 'review2.png',
+      name: 'Алёна',
+      review: 'Обратилась в АйтиШторм за помощью копирайтера. Ни разу ещё не пожалела! Ребята действительно вкладывают душу в то, что делают, и каждый текст, который я получаю, с нетерпением хочется выложить в сеть.'
+    },
+    {
+      image: 'review3.png',
+      name: 'Мария',
+      review: 'Команда АйтиШторма за такой короткий промежуток времени сделала невозможное: от простой фирмы по услуге продвижения выросла в мощный блог о важности личного бренда. Класс!'
+    }
+  ]
 
   constructor(private articlesService: ArticlesService) {
   }
